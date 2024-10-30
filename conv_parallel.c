53bf127e39ae8b7ac5ac4a95356e8b1e6912358a
buntpi <75521084+buntpi@users.noreply.github.com>
2024-10-30 16:11:50 +0700

    Update conv_parallel.c

--- conv_parallel.c
+++ conv_parallel.c
@@ -18,13 +18,12 @@ int main(){
 
     // implement here
     omp_set_num_threads(8);
-    #pragma omp parallel for
     int *FF = malloc(sizeof(int) * NF);
     int *R = malloc(sizeof(int) * (NA-NF+1));
 
     for(int i=0;i<NF;++i)
         FF[i]=F[NF-i-1];
-
+    #pragma omp parallel for
     for(int i=0;i<NA-NF+1;++i){
         int s=0;
         for(int j=0;j<NF;++j)
